5b20a32b029ed02d4dbd8b07ea9b8f5ecf3aefd2
alexeykomov <alexeykcontact@gmail.com>
2019-07-02 15:24:59 +0300

    edit: created workspace

---
+++ Note/Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Alex K on 7/2/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
